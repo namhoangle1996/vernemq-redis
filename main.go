c4421c22bfe5af5c9decbee15bcd68c06282b723
namhoangle <happybear02042000@gmail.com>
2020-05-25 15:02:59 +0700

    config back up redis

--- main.go
+++ main.go
@@ -4,42 +4,41 @@ import (
 	"encoding/json"
 	"github.com/go-redis/redis"
 	"golang.org/x/crypto/bcrypt"
+	"log"
 )
 
-func main()  {
+func main() {
 	client := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr:     "localhost:6379",
 		Password: "bbf5099c19728139a0c0bc77de98e241ddbe13a6f923b3557a6a035e45f05d1c27ec728af982b629b682d47fc043db5b48e2573880f3da3be527782ab7246181",
-		DB: 0,
+		DB:       0,
 	})
-
-	password := "namle123456"
+	log.Print(client.Ping())
+	password := "namle1234567890"
 	hash, _ := HashPassword(password)
 
 	maps := make([]map[string]string, 0)
 	mapSub := make([]map[string]string, 0)
 
-	type Value map[string]interface{}
+	//type Value map[string]interface{}
 
-	pattern := map[string]string{"pattern":"a/b/#"}
-	patterns := map[string]string{"pattern":"d/e/f/#"}
-	patternSub := map[string]string{"pattern":"hg/v1/#"}
-	maps = append(maps,pattern)
-	maps = append(maps,patterns)
-	mapSub = append(mapSub,patternSub)
-	mapSub = append(mapSub,pattern)
+	pattern := map[string]string{"pattern": "a/b/#"}
+	patterns := map[string]string{"pattern": "d/e/f/#"}
+	patternSub := map[string]string{"pattern": "hg/v1/#"}
+	maps = append(maps, pattern)
+	maps = append(maps, patterns)
+	mapSub = append(mapSub, patternSub)
+	mapSub = append(mapSub, pattern)
 
-	value := map[string]interface{}{"passhash": hash, "publish_acl": maps, "subscribe_acl": mapSub }
+	value := map[string]interface{}{"passhash": hash, "publish_acl": maps, "subscribe_acl": mapSub}
 
+	key := []string{"", "namle", "namle12345678"}
 
-	key := []string{"", "namle", "namle123456"}
-
-	bTest , err := json.Marshal(key)
-	bNono , err := json.Marshal(value)
-	if err != nil{
+	bTest, err := json.Marshal(key)
+	bNono, err := json.Marshal(value)
+	if err != nil {
 		panic(err)
 	}
-
 
 	err = client.Set(string(bTest),
 		bNono, 0).Err()
